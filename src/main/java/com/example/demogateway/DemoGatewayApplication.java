f10a2e7c0bbbf7c67c10ec4eb09d89474ccfaf1f
jackbauer123 <251908410@qq.com>
2023-01-22 17:44:17 +0800

    commit

---
+++ src/main/java/com/example/demogateway/DemoGatewayApplication.java
@@ -0,0 +1,23 @@
+package com.example.demogateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientId;
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientService;
+import org.springframework.util.Assert;
+import reactor.core.publisher.Mono;
+
+@SpringBootApplication
+public class DemoGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGatewayApplication.class, args);
+    }
+
+
+
+}
